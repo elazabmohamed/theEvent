83a8d43ab1e671f3afcd9241db6d6b5884d0f71a
Mohamed Elazab <66364977+elazabmohamed@users.noreply.github.com>
2020-12-25 18:05:04 +0300

    DB QUERY

--- SQL/Create Databae.sql
+++ SQL/Create Databae.sql
@@ -1,11 +1,11 @@
-if not exists (SELECT* FROM sys.databases WHERE name = 'THEVENT2') 
+﻿IF NOT EXISTS (SELECT* FROM sys.databases WHERE name = 'THEVENT2') 
 BEGIN 
-Create database [THEVENT2] 
+CREATE DATABASE [THEVENT2] 
 END
 GO
 USE [THEVENT2] 
 GO
-if not exists (SELECT* FROM sys.objects WHERE name = 'tblPerson')
+IF NOT EXISTS (SELECT* FROM sys.objects WHERE name = 'tblPerson')
 BEGIN 
 CREATE TABLE [dbo].[tblPerson](
 	[id] [int] IDENTITY(1,1) NOT NULL,
@@ -37,11 +37,11 @@ INSERT [dbo].[tblPerson] ([id], [type], [fname], [lname], [email], [password], [
 
 INSERT [dbo].[tblPerson] ([id], [type], [fname], [lname], [email], [password], [location], [birthdate], [registration]) VALUES (10, N'Member', N'Nurettin Resul', N'TANYILDIZI', N'resul@tanyildizi.com', N'resul', N'Elazig', CAST(N'1999-11-18' AS Date), CAST(N'2020-12-13T22:07:00' AS SmallDateTime))
 
-INSERT [dbo].[tblPerson] ([id], [type], [fname], [lname], [email], [password], [location], [birthdate], [registration]) VALUES (11, N'Member', N'Dogan Din�er', N'DEMIRCI', N'dogan@demirci.com', N'dogan', N'Amasya', CAST(N'2002-12-22' AS Date), CAST(N'2020-12-16T16:22:00' AS SmallDateTime))
+INSERT [dbo].[tblPerson] ([id], [type], [fname], [lname], [email], [password], [location], [birthdate], [registration]) VALUES (11, N'Member', N'Dogan Dinçer', N'DEMIRCI', N'dogan@demirci.com', N'dogan', N'Amasya', CAST(N'2002-12-22' AS Date), CAST(N'2020-12-16T16:22:00' AS SmallDateTime))
 SET IDENTITY_INSERT [dbo].[tblPerson] OFF 
 END
 
-if not exists (SELECT* FROM sys.objects WHERE name = 'tblEvent')
+IF NOT EXISTS (SELECT* FROM sys.objects WHERE name = 'tblEvent')
 BEGIN 
 CREATE TABLE [dbo].[tblEvent](
 	[eventid] [int] IDENTITY(1,1) NOT NULL,
@@ -58,14 +58,14 @@ CREATE TABLE [dbo].[tblEvent](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 SET IDENTITY_INSERT [dbo].[tblEvent] ON 
-INSERT [dbo].[tblEvent] ([eventid], [makerEmail], [eventName], [eventType], [eventLocation], [eventDate], [invitedMembers], [eventNote]) VALUES (5, N'nafiz@canitez.com', N'Football Match', N'Social', N'�anakkale', CAST(N'2019-12-20T08:30:00' AS SmallDateTime), N'
+INSERT [dbo].[tblEvent] ([eventid], [makerEmail], [eventName], [eventType], [eventLocation], [eventDate], [invitedMembers], [eventNote]) VALUES (5, N'nafiz@canitez.com', N'Football Match', N'Social', N'Çanakkale', CAST(N'2019-12-20T08:30:00' AS SmallDateTime), N'
 Erdem Demir.
 Yusuf Uslu.
 ', N'bring food')
 SET IDENTITY_INSERT [dbo].[tblEvent] OFF 
 END
 
-if not exists (SELECT* FROM sys.objects WHERE name = 'tblMessage')
+IF NOT EXISTS (SELECT* FROM sys.objects WHERE name = 'tblMessage')
 BEGIN 
 CREATE TABLE [dbo].[tblMessage](
 	[messageid] [int] IDENTITY(1,1) NOT NULL,
@@ -87,7 +87,7 @@ INSERT [dbo].[tblMessage] ([messageid], [senderid], [recieverid], [message], [me
 
 INSERT [dbo].[tblMessage] ([messageid], [senderid], [recieverid], [message], [messagedate]) VALUES (3, 3, 4, N'Ben Erdem', CAST(N'2020-12-08T19:12:02.057' AS DateTime))
 
-INSERT [dbo].[tblMessage] ([messageid], [senderid], [recieverid], [message], [messagedate]) VALUES (4, 2, 1, N'Nas?ls?n Elazab?', CAST(N'2020-12-08T19:13:02.057' AS DateTime))
+INSERT [dbo].[tblMessage] ([messageid], [senderid], [recieverid], [message], [messagedate]) VALUES (4, 2, 1, N'Nasılsın Elazab?', CAST(N'2020-12-08T19:13:02.057' AS DateTime))
 
 INSERT [dbo].[tblMessage] ([messageid], [senderid], [recieverid], [message], [messagedate]) VALUES (7, 2, 3, N'Erdem merhaba ben Nafiz', CAST(N'2020-12-08T19:16:02.057' AS DateTime))
 
@@ -95,14 +95,15 @@ INSERT [dbo].[tblMessage] ([messageid], [senderid], [recieverid], [message], [me
 
 INSERT [dbo].[tblMessage] ([messageid], [senderid], [recieverid], [message], [messagedate]) VALUES (10, 3, 2, N'erdem denem 123', CAST(N'2020-12-08T21:39:32.083' AS DateTime))
 
-INSERT [dbo].[tblMessage] ([messageid], [senderid], [recieverid], [message], [messagedate]) VALUES (11, 11, 2, N'Merhaba Nafiz ben Do?an', CAST(N'2020-12-16T17:08:22.897' AS DateTime))
+INSERT [dbo].[tblMessage] ([messageid], [senderid], [recieverid], [message], [messagedate]) VALUES (11, 11, 2, N'Merhaba Nafiz ben Doğan', CAST(N'2020-12-16T17:08:22.897' AS DateTime))
 
-INSERT [dbo].[tblMessage] ([messageid], [senderid], [recieverid], [message], [messagedate]) VALUES (12, 2, 3, N'Nas?ls?n Erdem?', CAST(N'2020-12-19T21:39:42.353' AS DateTime))
+INSERT [dbo].[tblMessage] ([messageid], [senderid], [recieverid], [message], [messagedate]) VALUES (12, 2, 3, N'Nasılsın Erdem?', CAST(N'2020-12-19T21:39:42.353' AS DateTime))
 
 SET IDENTITY_INSERT [dbo].[tblMessage] OFF
 
 END
-if not exists (SELECT* FROM sys.objects WHERE name = 'tblReview')
+
+IF NOT EXISTS (SELECT* FROM sys.objects WHERE name = 'tblReview')
 BEGIN 
 CREATE TABLE [dbo].[tblReview](
 	[reviewId] [int] IDENTITY(1,1) NOT NULL,
@@ -124,135 +125,92 @@ INSERT [dbo].[tblReview] ([reviewId], [reviewerId], [review], [reviewedEventId])
 
 SET IDENTITY_INSERT [dbo].[tblReview] OFF 
 
-
 END
 
 
-if not exists (SELECT* FROM sys.objects WHERE name = 'tblCity')
+IF NOT EXISTS (SELECT* FROM sys.objects WHERE name = 'tblCity')
 BEGIN 
 CREATE TABLE [dbo].[tblCity](
 	[city] [nvarchar](50) NULL
 ) ON [PRIMARY]
 
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Adana')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Ad?yaman')
-
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Adıyaman')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Afyonkarahisar')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'A?r?')
-
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Ağrı')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Aksaray')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Amasya')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Ankara')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Antalya')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Ardahan')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Artvin')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Ayd?n')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Bal?kesir')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Bart?n')
-
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Aydın')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Balıkesir')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Bartın')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Batman')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Bayburt')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Bilecik')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Bing�l')
-
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Bingöl')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Bitlis')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Bolu')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Burdur')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Bursa')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'�anakkale')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'�ank?r?')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'�orum')
-
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Çanakkale')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Çankırı')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Çorum')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Denizli')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Diyarbak?r')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'D�zce')
-
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Diyarbakır')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Düzce')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Edirne')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Elaz??')
-
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Elazığ')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Erzincan')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Erzurum')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Eski?ehir')
-
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Eskişehir')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Gaziantep')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Giresun')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'G�m�?hane')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Hakk�ri')
-
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Gümüşhane')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Hakkâri')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Hatay')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'I?d?r')
-
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Iğdır')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Isparta')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Istanbul')
-
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Izmir')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Kahramanmara?')
-
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Karab�k')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Kahramanmaraş')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Karabük')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Karaman')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Kars')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Kastamonu')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Kayseri')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Kilis')
-INSERT [dbo].[tblCity] ([city]) VALUES (N'K?r?kkale')
-INSERT [dbo].[tblCity] ([city]) VALUES (N'K?rklareli')
-INSERT [dbo].[tblCity] ([city]) VALUES (N'K?r?ehir')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Kırıkkale')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Kırklareli')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Kırşehir')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Kocaeli')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Konya')
-INSERT [dbo].[tblCity] ([city]) VALUES (N'K�tahya')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Kütahya')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Malatya')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Manisa')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Mardin')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Mersin')
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Mu?la')
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Mu?')
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Nev?ehir')
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Ni?de')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Muğla')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Muş')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Nevşehir')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Niğde')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Ordu')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Osmaniye')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Rize')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Sakarya')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Samsun')
-INSERT [dbo].[tblCity] ([city]) VALUES (N'?anl?urfa')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Şanlıurfa')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Siirt')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Sinop')
-INSERT [dbo].[tblCity] ([city]) VALUES (N'??rnak')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Şırnak')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Sivas')
-INSERT [dbo].[tblCity] ([city]) VALUES (N'Tekirda?')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Tekirdağ')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Tokat')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Trabzon')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Tunceli')
-INSERT [dbo].[tblCity] ([city]) VALUES (N'U?ak')
+INSERT [dbo].[tblCity] ([city]) VALUES (N'Uşak')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Van')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Yalova')
 INSERT [dbo].[tblCity] ([city]) VALUES (N'Yozgat')
